c72fad266309ff95de72d6597ff74751009f0b2d
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-06-07 16:02:39 +0200

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -9,3 +9,24 @@ function App() {
 }
 
 export default App;
+
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
