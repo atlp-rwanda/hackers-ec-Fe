41465736ce0bf1020191359c4a0403dbba3838e8
Hakizimana Clement <h.clementoert@gmail.com>
2024-06-10 12:20:12 +0200

    chore(readme): add all badges technologies
    [Delivers #187419042]

---
+++ src/test/Nav.test.tsx
@@ -0,0 +1,25 @@
+import Nav from "../components/Nav";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { describe, it, expect } from "vitest";
+
+describe("Nav Component", () => {
+  it("renders the Home link", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+  });
+
+  it("renders the About link", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("About")).toBeInTheDocument();
+  });
+});
