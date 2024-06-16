2c81f727738db1954a891443f0aeeb947e7ed780
kwizera-bonheur25 <kwizerabonheur25@gmail.com>
2024-06-16 04:39:07 +0200

    feat(verify-email):User should be able to verify the email

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import loginSlice from './features/loginSlice';
 import RegisterSlice from './features/RegisterSlice';
 import navReducer from './features/navSlice';
+import VerifyAccountSlice from './features/VerifyAccountSlice';
 export const store = configureStore({
 	reducer: {
 		login: loginSlice,
 		register: RegisterSlice,
 		nav: navReducer,
+		verifyAccount: VerifyAccountSlice,
 	},
 });
 
